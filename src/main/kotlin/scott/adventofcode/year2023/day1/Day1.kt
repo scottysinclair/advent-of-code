ade217ce861312237b0d2c3a7d7ae050014e6208
exssinclair <scott.sinclair@extern.wrwks.at>
2023-12-04 19:22:46 +0100

    day 1 success

--- src/main/kotlin/scott/adventofcode/year2023/day1/Day1.kt
+++ src/main/kotlin/scott/adventofcode/year2023/day1/Day1.kt
@@ -1,76 +1,47 @@
 package scott.adventofcode.year2023.day1
 
 import java.io.File
+import java.lang.Integer.min
 import java.util.regex.Pattern
 
 fun main() {
    File("src/main/resources/day-1-input.txt").useLines { lines ->
      lines.map { line ->
-        val processedLine = preprocess(line.lowercase())
-        val number = calculateNumber(processedLine)
-        println("$line  =>  $processedLine  => $number")
-        number
+        runCatching {
+           val number = calculateNumber(line)
+           println("$line  => $number")
+           number
+        }.onFailure { println("Error processing line $line") }
+           .getOrThrow()
      }.sum()
    }.let(::println)
 }
 
-fun calculateNumber(line: String) : Long {
-   val first = line.first { it.isDigit() }
-   val last = line.last { it.isDigit() }
-   return "$first$last".toLong()
+fun calculateNumber(line: String) : Int {
+   val first = getFirstDigit(line, numberText)
+   val last = getLastDigit(line, numberText)
+   return "$first$last".toInt()
 }
 
-class Result(val index: Int, val numberText: String, val digit: Char)
-fun preprocess(line: String) : String {
-   /*
-    * for each index in the line (from left to right)
-    */
-   val result = (0..line.length).firstNotNullOfOrNull { index ->
-      /*
-       * check if we can  replace at that point a number word with a digit
-       */
-      replaceWithDigit(line, index)?.let {
-         /*
-          * if so return the details
-          */
-         Result(index, it.first, it.second)
+fun getFirstDigit(line: String, numberText: List<String>): Int {
+   return line.indices.firstNotNullOf { i ->
+      if (line[i] in '0'..'9') {
+         line[i].toString().toInt()
+      }
+      else {
+         numberText.indices.firstNotNullOfOrNull { nti ->
+            val linePart = line.substring(i, min(i + numberText[nti].length, line.length))
+            if (linePart == numberText[nti]) nti+1
+            else null
+         }
       }
    }
-   /*
-    * If no result then the line is not modified
-    */
-   return if (result == null) line
-   else {
-      /*
-       * otherwise swap out the number for the digit and recursively process
-       */
-      val before = line.substring(0, result.index)
-      val after = line.substring(result.index + result.numberText.length, line.length)
-      preprocess("$before${result.digit}$after")
-   }
+}
+fun getLastDigit(line: String, numberText: List<String>): Int {
+   return getFirstDigit(line.reversed(), numberText.map { it.reversed() })
 }
 
 
-val numbers = listOf(
-   "one" to '1',
-   "two" to '2',
-   "three" to '3',
-   "four" to '4',
-   "five" to '5',
-   "six" to '6',
-   "seven" to '7',
-   "eight" to '8',
-   "nine" to '9'
-)
-
-/**
- * Returns the digit that can be replaced at that character
- */
-fun replaceWithDigit(line: String, index: Int) : Pair<String,Char>? {
-   return numbers.firstOrNull {(text, digit) ->
-       line.regionMatches(index, text, 0, text.length)
-   }
-}
-
+private val numberText = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 
 
