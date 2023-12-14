7dc2e0d28e8e27f20cd6ff850a7d94d61ccca9e0
exssinclair <scott.sinclair@extern.wrwks.at>
2023-12-14 19:14:47 +0100

    Day 5 ✅

--- src/main/kotlin/scott/adventofcode/year2023/day5/Day5.kt
+++ src/main/kotlin/scott/adventofcode/year2023/day5/Day5.kt
@@ -1,17 +1,19 @@
 package scott.adventofcode.year2023.day5
 
 import java.io.File
+import java.math.RoundingMode
+import java.util.concurrent.atomic.AtomicLong
 
 fun main() {
-   part1()
-   //part2()
+   //part1()
+   part2()
 }
 
 fun part1() {
    File("src/main/resources/day-5-input.txt").useLines { linesSeq ->
       val lines = linesSeq.toList()
       val resourceFactory = ResourceFactory()
-      val initialSeeds = parseInitialSeeds(lines)
+      val initialSeeds = parseInitialSeedsForPart1(lines)
       val resourceMappings = parseResourceMappings(lines, resourceFactory)
       resourceMappings.forEach { println(it) }
 
@@ -19,17 +21,56 @@ fun part1() {
       val location = resourceFactory.get("location")
       val resourceChain = buildResourceChain(seed, location, resourceMappings)
 
-      initialSeeds.map { seed ->
+      initialSeeds.minOf { seed ->
          println("Processing seed $seed -----------------")
-         resourceChain.fold(seed) { number, nextMapping ->
-            nextMapping.map(number).also {
-               println("Mapping $number from ${nextMapping.source.name} to ${nextMapping.destination.name} => $it")
+         resourceChain.fold(seed) { number, nextResourceMapping ->
+            nextResourceMapping.map(number).also {
+               println("Mapping $number from ${nextResourceMapping.source.name} to ${nextResourceMapping.destination.name} => $it")
             }
          }
-      }.min().let(::println)
-
+      }.let(::println)
    }
 }
+
+fun part2() {
+   File("src/main/resources/day-5-input.txt").useLines { linesSeq ->
+      val lines = linesSeq.toList()
+      val resourceFactory = ResourceFactory()
+      val initialSeedRanges = parseInitialSeedsForPart2(lines)
+      initialSeedRanges.forEach { println(it) }
+      val resourceMappings = parseResourceMappings(lines, resourceFactory)
+      resourceMappings.forEach { println(it) }
+
+      val seed = resourceFactory.get("seed")
+      val location = resourceFactory.get("location")
+      val resourceChain = buildResourceChain(seed, location, resourceMappings)
+
+      val totalSeeds = initialSeedRanges.sumOf { it.length }
+      val totalLocations = resourceMappings.first { it.destination == location }.length()
+
+      println("total seeds: $totalSeeds")
+      println("total locations: $totalLocations")
+
+      val count = AtomicLong(0)
+      initialSeedRanges.parallelStream().map { seedRange ->
+         seedRange.minOf { seed ->
+            printProgress(count, totalSeeds)
+            resourceChain.fold(seed) { number, nextResourceMapping -> nextResourceMapping.map(number)  }
+         }
+      }.toList().min().let(::println)
+   }
+}
+
+fun printProgress(count: AtomicLong, totalSeeds: Long) {
+   val myCount = count.incrementAndGet()
+   if (myCount % (1000000 * 30) == 0L) {
+      val percentComplete = ((myCount / totalSeeds.toDouble()) * 100).toBigDecimal().setScale(2, RoundingMode.HALF_EVEN)
+      println("Count $myCount from $totalSeeds  => $percentComplete%")
+   }
+}
+
+
+
 
 fun buildResourceChain(from: Resource, to: Resource, resourceMappings: List<ResourceMappings>) : List<ResourceMappings> {
    val chain = mutableListOf<ResourceMappings>()
@@ -42,12 +83,25 @@ fun buildResourceChain(from: Resource, to: Resource, resourceMappings: List<Reso
    return chain
 }
 
-fun parseInitialSeeds(lines: List<String>) : List<Long>  = lines[0].substring("seeds: ".length).split(" ").map { it.trim().toLong() }
+fun parseInitialSeedsForPart1(lines: List<String>) : List<Long>  = lines[0].substring("seeds: ".length).split(" ").map { it.trim().toLong() }
+fun parseInitialSeedsForPart2(lines: List<String>) : List<SeedRange> {
+   val numbers = parseInitialSeedsForPart1(lines)
+   return numbers.mapIndexedNotNull { index, number ->
+      if (index % 2 == 0) SeedRange(number, numbers[index + 1])
+      else null
+   }
+}
+
+data class SeedRange(val startSeed: Long, val length: Long) : Iterable<Long> {
+   override fun iterator(): Iterator<Long> {
+      return LongRange(startSeed, startSeed + length - 1).iterator()
+   }
+}
 
 class ResourceFactory {
    private val resources = mutableMapOf<String, Resource>()
    fun get(name: String) : Resource {
-      return resources.get(name)!!
+      return resources[name]!!
    }
    fun getOrCreate(name: String) : Resource {
       return resources.getOrPut(name) { Resource(name) }
@@ -100,6 +154,9 @@ class ResourceMappings(val source: Resource, val destination: Resource) {
    fun map(input: Long) : Long {
       return ranges.firstNotNullOfOrNull { r -> r.map(input) } ?: input
    }
+   fun length() : Long {
+      return ranges.sumOf { it.length() }
+   }
 
    override fun toString(): String {
       return "ResourceMappings(source=$source, destination=$destination, #ranges=${ranges.size})"
@@ -112,13 +169,14 @@ class MappedRange(private val sourceRange: Range, private val destination: Range
    fun map(input: Long) : Long? {
       return sourceRange.indexOf(input)?.let { destination[it] }
    }
+   fun length() : Long = sourceRange.length
 }
 
 class Range(val from: Long, val length: Long) {
    fun indexOf(input: Long) : Long? {
       if (input < from) return null
-      val endExlusive = from + length
-      if (input >= endExlusive) return null
+      val endExclusive = from + length
+      if (input >= endExclusive) return null
       return input - from
    }
 
